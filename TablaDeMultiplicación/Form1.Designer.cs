a0fbb750424388e1a316990aaf5114f565adb255
Godspell34 <165835182+Godspell34@users.noreply.github.com>
2024-11-08 13:45:37 -0600

    Add files via upload

---
+++ TablaDeMultiplicación/Form1.Designer.cs
@@ -0,0 +1,147 @@
+﻿namespace TablaDeMultiplicación
+{
+    partial class Form1
+    {
+        /// <summary>
+        /// Variable del diseñador necesaria.
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        /// Limpiar los recursos que se estén usando.
+        /// </summary>
+        /// <param name="disposing">true si los recursos administrados se deben desechar; false en caso contrario.</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Código generado por el Diseñador de Windows Forms
+
+        /// <summary>
+        /// Método necesario para admitir el Diseñador. No se puede modificar
+        /// el contenido de este método con el editor de código.
+        /// </summary>
+        private void InitializeComponent()
+        {
+            this.lblIngreseUnNumero = new System.Windows.Forms.Label();
+            this.txtIngresarNumero = new System.Windows.Forms.TextBox();
+            this.lstTabla = new System.Windows.Forms.ListBox();
+            this.btnCalcular = new System.Windows.Forms.Button();
+            this.btnLimpiar = new System.Windows.Forms.Button();
+            this.btnSalir = new System.Windows.Forms.Button();
+            this.label1 = new System.Windows.Forms.Label();
+            this.SuspendLayout();
+            // 
+            // lblIngreseUnNumero
+            // 
+            this.lblIngreseUnNumero.AutoSize = true;
+            this.lblIngreseUnNumero.Location = new System.Drawing.Point(47, 50);
+            this.lblIngreseUnNumero.Name = "lblIngreseUnNumero";
+            this.lblIngreseUnNumero.Size = new System.Drawing.Size(167, 22);
+            this.lblIngreseUnNumero.TabIndex = 0;
+            this.lblIngreseUnNumero.Text = "Ingrese un Número:";
+            // 
+            // txtIngresarNumero
+            // 
+            this.txtIngresarNumero.BackColor = System.Drawing.SystemColors.WindowText;
+            this.txtIngresarNumero.Font = new System.Drawing.Font("Microsoft Sans Serif", 10.8F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.txtIngresarNumero.ForeColor = System.Drawing.SystemColors.Window;
+            this.txtIngresarNumero.Location = new System.Drawing.Point(220, 47);
+            this.txtIngresarNumero.Name = "txtIngresarNumero";
+            this.txtIngresarNumero.Size = new System.Drawing.Size(143, 28);
+            this.txtIngresarNumero.TabIndex = 1;
+            this.txtIngresarNumero.TextChanged += new System.EventHandler(this.txtIngresarNumero_TextChanged);
+            this.txtIngresarNumero.KeyPress += new System.Windows.Forms.KeyPressEventHandler(this.txtIngresarNumero_KeyPress);
+            // 
+            // lstTabla
+            // 
+            this.lstTabla.BackColor = System.Drawing.SystemColors.Info;
+            this.lstTabla.FormattingEnabled = true;
+            this.lstTabla.ItemHeight = 22;
+            this.lstTabla.Location = new System.Drawing.Point(51, 81);
+            this.lstTabla.Name = "lstTabla";
+            this.lstTabla.Size = new System.Drawing.Size(312, 378);
+            this.lstTabla.TabIndex = 2;
+            // 
+            // btnCalcular
+            // 
+            this.btnCalcular.BackColor = System.Drawing.Color.FromArgb(((int)(((byte)(255)))), ((int)(((byte)(255)))), ((int)(((byte)(192)))));
+            this.btnCalcular.Location = new System.Drawing.Point(51, 465);
+            this.btnCalcular.Name = "btnCalcular";
+            this.btnCalcular.Size = new System.Drawing.Size(124, 64);
+            this.btnCalcular.TabIndex = 3;
+            this.btnCalcular.Text = "Calcular";
+            this.btnCalcular.UseVisualStyleBackColor = false;
+            this.btnCalcular.Click += new System.EventHandler(this.btnCalcular_Click);
+            // 
+            // btnLimpiar
+            // 
+            this.btnLimpiar.BackColor = System.Drawing.Color.FromArgb(((int)(((byte)(255)))), ((int)(((byte)(255)))), ((int)(((byte)(192)))));
+            this.btnLimpiar.Location = new System.Drawing.Point(220, 465);
+            this.btnLimpiar.Name = "btnLimpiar";
+            this.btnLimpiar.Size = new System.Drawing.Size(124, 64);
+            this.btnLimpiar.TabIndex = 4;
+            this.btnLimpiar.Text = "Limpiar";
+            this.btnLimpiar.UseVisualStyleBackColor = false;
+            this.btnLimpiar.Click += new System.EventHandler(this.btnLimpiar_Click);
+            // 
+            // btnSalir
+            // 
+            this.btnSalir.BackColor = System.Drawing.Color.FromArgb(((int)(((byte)(255)))), ((int)(((byte)(255)))), ((int)(((byte)(192)))));
+            this.btnSalir.Location = new System.Drawing.Point(136, 535);
+            this.btnSalir.Name = "btnSalir";
+            this.btnSalir.Size = new System.Drawing.Size(124, 64);
+            this.btnSalir.TabIndex = 5;
+            this.btnSalir.Text = "Salir";
+            this.btnSalir.UseVisualStyleBackColor = false;
+            this.btnSalir.Click += new System.EventHandler(this.btnSalir_Click);
+            // 
+            // label1
+            // 
+            this.label1.AutoSize = true;
+            this.label1.Font = new System.Drawing.Font("Microsoft Sans Serif", 16.2F, ((System.Drawing.FontStyle)((System.Drawing.FontStyle.Bold | System.Drawing.FontStyle.Italic))), System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.label1.Location = new System.Drawing.Point(23, 12);
+            this.label1.Name = "label1";
+            this.label1.Size = new System.Drawing.Size(366, 32);
+            this.label1.TabIndex = 6;
+            this.label1.Text = "\"Tablas de multiplicacion\"";
+            // 
+            // Form1
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(10F, 22F);
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.BackColor = System.Drawing.Color.Cyan;
+            this.ClientSize = new System.Drawing.Size(401, 619);
+            this.Controls.Add(this.label1);
+            this.Controls.Add(this.btnSalir);
+            this.Controls.Add(this.btnLimpiar);
+            this.Controls.Add(this.btnCalcular);
+            this.Controls.Add(this.lstTabla);
+            this.Controls.Add(this.txtIngresarNumero);
+            this.Controls.Add(this.lblIngreseUnNumero);
+            this.Font = new System.Drawing.Font("Microsoft Sans Serif", 10.8F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.Margin = new System.Windows.Forms.Padding(4);
+            this.Name = "Form1";
+            this.Text = "Tabla de Multiplicación";
+            this.ResumeLayout(false);
+            this.PerformLayout();
+
+        }
+
+        #endregion
+
+        private System.Windows.Forms.Label lblIngreseUnNumero;
+        private System.Windows.Forms.TextBox txtIngresarNumero;
+        private System.Windows.Forms.ListBox lstTabla;
+        private System.Windows.Forms.Button btnCalcular;
+        private System.Windows.Forms.Button btnLimpiar;
+        private System.Windows.Forms.Button btnSalir;
+        private System.Windows.Forms.Label label1;
+    }
+}
+
---
+++ TablaDeMultiplicación/Form1.cs
@@ -0,0 +1,56 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+
+namespace TablaDeMultiplicación
+{
+    public partial class Form1 : Form
+    {
+        public Form1()
+        {
+            InitializeComponent();
+        }
+
+        private void txtIngresarNumero_KeyPress(object sender, KeyPressEventArgs e)
+        {
+            if ( !char.IsDigit(e.KeyChar) && !char.IsControl(e.KeyChar) )
+            {
+                e.Handled = true;
+            }
+        }
+
+        private void btnSalir_Click(object sender, EventArgs e)
+        {
+           Close();
+        }
+
+        private void btnLimpiar_Click(object sender, EventArgs e)
+        {
+            txtIngresarNumero.Clear();
+            lstTabla.Items.Clear();
+        }
+
+        private void btnCalcular_Click(object sender, EventArgs e)
+        {
+            int i, numero, resultado;
+
+            numero = int.Parse(txtIngresarNumero.Text);
+            for (i = 1; i <= 10; i++)
+            {
+                resultado = numero * i;
+                lstTabla.Items.Add(numero + " * " + i + "=  " + resultado);
+            }
+        }
+
+        private void txtIngresarNumero_TextChanged(object sender, EventArgs e)
+        {
+
+        }
+    }
+}
