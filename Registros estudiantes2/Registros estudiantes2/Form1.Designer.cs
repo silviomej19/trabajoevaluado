5092ed6bd852051980b232fb678bc3980b35fa2d
Godspell34 <165835182+Godspell34@users.noreply.github.com>
2024-11-15 09:59:54 -0600

    Add files via upload

---
+++ Registros estudiantes2/Registros estudiantes2/Form1.Designer.cs
@@ -0,0 +1,411 @@
+﻿namespace Registros_estudiantes2
+{
+    partial class Form1
+    {
+        /// <summary>
+        /// Variable del diseñador necesaria.
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        /// Limpiar los recursos que se estén usando.
+        /// </summary>
+        /// <param name="disposing">true si los recursos administrados se deben desechar; false en caso contrario.</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Código generado por el Diseñador de Windows Forms
+
+        /// <summary>
+        /// Método necesario para admitir el Diseñador. No se puede modificar
+        /// el contenido de este método con el editor de código.
+        /// </summary>
+        private void InitializeComponent()
+        {
+            this.label1 = new System.Windows.Forms.Label();
+            this.label2 = new System.Windows.Forms.Label();
+            this.label3 = new System.Windows.Forms.Label();
+            this.label4 = new System.Windows.Forms.Label();
+            this.label5 = new System.Windows.Forms.Label();
+            this.label6 = new System.Windows.Forms.Label();
+            this.textBox1 = new System.Windows.Forms.TextBox();
+            this.textBox2 = new System.Windows.Forms.TextBox();
+            this.comboBox1 = new System.Windows.Forms.ComboBox();
+            this.textBox3 = new System.Windows.Forms.TextBox();
+            this.textBox4 = new System.Windows.Forms.TextBox();
+            this.textBox5 = new System.Windows.Forms.TextBox();
+            this.dataGridView1 = new System.Windows.Forms.DataGridView();
+            this.Nombre = new System.Windows.Forms.DataGridViewTextBoxColumn();
+            this.Apellido = new System.Windows.Forms.DataGridViewTextBoxColumn();
+            this.Carrera = new System.Windows.Forms.DataGridViewTextBoxColumn();
+            this.CIF = new System.Windows.Forms.DataGridViewTextBoxColumn();
+            this.PromedioGeneral = new System.Windows.Forms.DataGridViewTextBoxColumn();
+            this.MateriasAprobadas = new System.Windows.Forms.DataGridViewTextBoxColumn();
+            this.BTNguardar = new System.Windows.Forms.Button();
+            this.BTNeliminar = new System.Windows.Forms.Button();
+            this.BTNactualizar = new System.Windows.Forms.Button();
+            this.menuStrip1 = new System.Windows.Forms.MenuStrip();
+            this.archivoToolStripMenuItem = new System.Windows.Forms.ToolStripMenuItem();
+            this.nuevoToolStripMenuItem = new System.Windows.Forms.ToolStripMenuItem();
+            this.salirToolStripMenuItem = new System.Windows.Forms.ToolStripMenuItem();
+            this.estudainteToolStripMenuItem = new System.Windows.Forms.ToolStripMenuItem();
+            this.guardarToolStripMenuItem = new System.Windows.Forms.ToolStripMenuItem();
+            this.eliminarToolStripMenuItem = new System.Windows.Forms.ToolStripMenuItem();
+            this.ayudaToolStripMenuItem = new System.Windows.Forms.ToolStripMenuItem();
+            this.acercaDeToolStripMenuItem = new System.Windows.Forms.ToolStripMenuItem();
+            ((System.ComponentModel.ISupportInitialize)(this.dataGridView1)).BeginInit();
+            this.menuStrip1.SuspendLayout();
+            this.SuspendLayout();
+            // 
+            // label1
+            // 
+            this.label1.AutoSize = true;
+            this.label1.Location = new System.Drawing.Point(7, 85);
+            this.label1.Name = "label1";
+            this.label1.Size = new System.Drawing.Size(63, 16);
+            this.label1.TabIndex = 0;
+            this.label1.Text = "Nombres";
+            // 
+            // label2
+            // 
+            this.label2.AutoSize = true;
+            this.label2.Location = new System.Drawing.Point(317, 85);
+            this.label2.Name = "label2";
+            this.label2.Size = new System.Drawing.Size(64, 16);
+            this.label2.TabIndex = 1;
+            this.label2.Text = "Apellidos";
+            // 
+            // label3
+            // 
+            this.label3.AutoSize = true;
+            this.label3.Location = new System.Drawing.Point(617, 85);
+            this.label3.Name = "label3";
+            this.label3.Size = new System.Drawing.Size(52, 16);
+            this.label3.TabIndex = 2;
+            this.label3.Text = "Carrera";
+            this.label3.Click += new System.EventHandler(this.label3_Click);
+            // 
+            // label4
+            // 
+            this.label4.AutoSize = true;
+            this.label4.Location = new System.Drawing.Point(7, 131);
+            this.label4.Name = "label4";
+            this.label4.Size = new System.Drawing.Size(27, 16);
+            this.label4.TabIndex = 3;
+            this.label4.Text = "CIF";
+            // 
+            // label5
+            // 
+            this.label5.AutoSize = true;
+            this.label5.Location = new System.Drawing.Point(317, 137);
+            this.label5.Name = "label5";
+            this.label5.Size = new System.Drawing.Size(109, 16);
+            this.label5.TabIndex = 4;
+            this.label5.Text = "Promedio Genral";
+            // 
+            // label6
+            // 
+            this.label6.AutoSize = true;
+            this.label6.Location = new System.Drawing.Point(555, 137);
+            this.label6.Name = "label6";
+            this.label6.Size = new System.Drawing.Size(130, 16);
+            this.label6.TabIndex = 5;
+            this.label6.Text = "Materias Aprobadas";
+            // 
+            // textBox1
+            // 
+            this.textBox1.Location = new System.Drawing.Point(115, 82);
+            this.textBox1.Name = "textBox1";
+            this.textBox1.Size = new System.Drawing.Size(100, 22);
+            this.textBox1.TabIndex = 6;
+            this.textBox1.TextChanged += new System.EventHandler(this.textBox1_TextChanged);
+            // 
+            // textBox2
+            // 
+            this.textBox2.Location = new System.Drawing.Point(443, 85);
+            this.textBox2.Name = "textBox2";
+            this.textBox2.Size = new System.Drawing.Size(100, 22);
+            this.textBox2.TabIndex = 7;
+            this.textBox2.TextChanged += new System.EventHandler(this.textBox2_TextChanged);
+            // 
+            // comboBox1
+            // 
+            this.comboBox1.FormattingEnabled = true;
+            this.comboBox1.Items.AddRange(new object[] {
+            "Ingieneria",
+            "Medicina",
+            "Derecho",
+            "Administracion"});
+            this.comboBox1.Location = new System.Drawing.Point(670, 83);
+            this.comboBox1.Name = "comboBox1";
+            this.comboBox1.Size = new System.Drawing.Size(121, 24);
+            this.comboBox1.TabIndex = 8;
+            this.comboBox1.SelectedIndexChanged += new System.EventHandler(this.comboBox1_SelectedIndexChanged);
+            // 
+            // textBox3
+            // 
+            this.textBox3.Location = new System.Drawing.Point(115, 131);
+            this.textBox3.Name = "textBox3";
+            this.textBox3.Size = new System.Drawing.Size(100, 22);
+            this.textBox3.TabIndex = 9;
+            this.textBox3.TextChanged += new System.EventHandler(this.textBox3_TextChanged);
+            // 
+            // textBox4
+            // 
+            this.textBox4.Location = new System.Drawing.Point(443, 131);
+            this.textBox4.Name = "textBox4";
+            this.textBox4.Size = new System.Drawing.Size(100, 22);
+            this.textBox4.TabIndex = 10;
+            this.textBox4.TextChanged += new System.EventHandler(this.textBox4_TextChanged);
+            // 
+            // textBox5
+            // 
+            this.textBox5.Location = new System.Drawing.Point(691, 134);
+            this.textBox5.Name = "textBox5";
+            this.textBox5.Size = new System.Drawing.Size(100, 22);
+            this.textBox5.TabIndex = 11;
+            this.textBox5.TextChanged += new System.EventHandler(this.textBox5_TextChanged);
+            // 
+            // dataGridView1
+            // 
+            this.dataGridView1.AllowUserToAddRows = false;
+            this.dataGridView1.ColumnHeadersHeightSizeMode = System.Windows.Forms.DataGridViewColumnHeadersHeightSizeMode.AutoSize;
+            this.dataGridView1.Columns.AddRange(new System.Windows.Forms.DataGridViewColumn[] {
+            this.Nombre,
+            this.Apellido,
+            this.Carrera,
+            this.CIF,
+            this.PromedioGeneral,
+            this.MateriasAprobadas});
+            this.dataGridView1.Location = new System.Drawing.Point(12, 217);
+            this.dataGridView1.Name = "dataGridView1";
+            this.dataGridView1.ReadOnly = true;
+            this.dataGridView1.RowHeadersWidth = 51;
+            this.dataGridView1.RowTemplate.Height = 24;
+            this.dataGridView1.Size = new System.Drawing.Size(769, 221);
+            this.dataGridView1.TabIndex = 12;
+            // 
+            // Nombre
+            // 
+            this.Nombre.HeaderText = "Nombre";
+            this.Nombre.MinimumWidth = 6;
+            this.Nombre.Name = "Nombre";
+            this.Nombre.ReadOnly = true;
+            this.Nombre.Width = 125;
+            // 
+            // Apellido
+            // 
+            this.Apellido.HeaderText = "Apellido";
+            this.Apellido.MinimumWidth = 6;
+            this.Apellido.Name = "Apellido";
+            this.Apellido.ReadOnly = true;
+            this.Apellido.Width = 125;
+            // 
+            // Carrera
+            // 
+            this.Carrera.HeaderText = "Carrera";
+            this.Carrera.MinimumWidth = 6;
+            this.Carrera.Name = "Carrera";
+            this.Carrera.ReadOnly = true;
+            this.Carrera.Width = 125;
+            // 
+            // CIF
+            // 
+            this.CIF.HeaderText = "CIF";
+            this.CIF.MinimumWidth = 6;
+            this.CIF.Name = "CIF";
+            this.CIF.ReadOnly = true;
+            this.CIF.Width = 125;
+            // 
+            // PromedioGeneral
+            // 
+            this.PromedioGeneral.HeaderText = "PromedioGeneral";
+            this.PromedioGeneral.MinimumWidth = 6;
+            this.PromedioGeneral.Name = "PromedioGeneral";
+            this.PromedioGeneral.ReadOnly = true;
+            this.PromedioGeneral.Width = 125;
+            // 
+            // MateriasAprobadas
+            // 
+            this.MateriasAprobadas.HeaderText = "MateriasAprobadas";
+            this.MateriasAprobadas.MinimumWidth = 6;
+            this.MateriasAprobadas.Name = "MateriasAprobadas";
+            this.MateriasAprobadas.ReadOnly = true;
+            this.MateriasAprobadas.Width = 125;
+            // 
+            // BTNguardar
+            // 
+            this.BTNguardar.Location = new System.Drawing.Point(115, 178);
+            this.BTNguardar.Name = "BTNguardar";
+            this.BTNguardar.Size = new System.Drawing.Size(75, 23);
+            this.BTNguardar.TabIndex = 13;
+            this.BTNguardar.Text = "Guardar";
+            this.BTNguardar.UseVisualStyleBackColor = true;
+            this.BTNguardar.Click += new System.EventHandler(this.button1_Click);
+            // 
+            // BTNeliminar
+            // 
+            this.BTNeliminar.Location = new System.Drawing.Point(357, 178);
+            this.BTNeliminar.Name = "BTNeliminar";
+            this.BTNeliminar.Size = new System.Drawing.Size(75, 23);
+            this.BTNeliminar.TabIndex = 14;
+            this.BTNeliminar.Text = "Eliminar";
+            this.BTNeliminar.UseVisualStyleBackColor = true;
+            this.BTNeliminar.Click += new System.EventHandler(this.BTNeliminar_Click);
+            // 
+            // BTNactualizar
+            // 
+            this.BTNactualizar.Location = new System.Drawing.Point(644, 178);
+            this.BTNactualizar.Name = "BTNactualizar";
+            this.BTNactualizar.Size = new System.Drawing.Size(87, 23);
+            this.BTNactualizar.TabIndex = 15;
+            this.BTNactualizar.Text = "Actualizar";
+            this.BTNactualizar.UseVisualStyleBackColor = true;
+            this.BTNactualizar.Click += new System.EventHandler(this.BTNactualizar_Click);
+            // 
+            // menuStrip1
+            // 
+            this.menuStrip1.ImageScalingSize = new System.Drawing.Size(20, 20);
+            this.menuStrip1.Items.AddRange(new System.Windows.Forms.ToolStripItem[] {
+            this.archivoToolStripMenuItem,
+            this.estudainteToolStripMenuItem,
+            this.ayudaToolStripMenuItem});
+            this.menuStrip1.Location = new System.Drawing.Point(0, 0);
+            this.menuStrip1.Name = "menuStrip1";
+            this.menuStrip1.Size = new System.Drawing.Size(800, 28);
+            this.menuStrip1.TabIndex = 16;
+            this.menuStrip1.Text = "menuStrip1";
+            // 
+            // archivoToolStripMenuItem
+            // 
+            this.archivoToolStripMenuItem.DropDownItems.AddRange(new System.Windows.Forms.ToolStripItem[] {
+            this.nuevoToolStripMenuItem,
+            this.salirToolStripMenuItem});
+            this.archivoToolStripMenuItem.Name = "archivoToolStripMenuItem";
+            this.archivoToolStripMenuItem.Size = new System.Drawing.Size(73, 24);
+            this.archivoToolStripMenuItem.Text = "Archivo";
+            // 
+            // nuevoToolStripMenuItem
+            // 
+            this.nuevoToolStripMenuItem.Name = "nuevoToolStripMenuItem";
+            this.nuevoToolStripMenuItem.Size = new System.Drawing.Size(135, 26);
+            this.nuevoToolStripMenuItem.Text = "Nuevo";
+            // 
+            // salirToolStripMenuItem
+            // 
+            this.salirToolStripMenuItem.Name = "salirToolStripMenuItem";
+            this.salirToolStripMenuItem.Size = new System.Drawing.Size(135, 26);
+            this.salirToolStripMenuItem.Text = "Salir";
+            this.salirToolStripMenuItem.Click += new System.EventHandler(this.salirToolStripMenuItem_Click);
+            // 
+            // estudainteToolStripMenuItem
+            // 
+            this.estudainteToolStripMenuItem.DropDownItems.AddRange(new System.Windows.Forms.ToolStripItem[] {
+            this.guardarToolStripMenuItem,
+            this.eliminarToolStripMenuItem});
+            this.estudainteToolStripMenuItem.Name = "estudainteToolStripMenuItem";
+            this.estudainteToolStripMenuItem.Size = new System.Drawing.Size(92, 24);
+            this.estudainteToolStripMenuItem.Text = "Estudiante";
+            // 
+            // guardarToolStripMenuItem
+            // 
+            this.guardarToolStripMenuItem.Name = "guardarToolStripMenuItem";
+            this.guardarToolStripMenuItem.Size = new System.Drawing.Size(146, 26);
+            this.guardarToolStripMenuItem.Text = "Guardar";
+            // 
+            // eliminarToolStripMenuItem
+            // 
+            this.eliminarToolStripMenuItem.Name = "eliminarToolStripMenuItem";
+            this.eliminarToolStripMenuItem.Size = new System.Drawing.Size(146, 26);
+            this.eliminarToolStripMenuItem.Text = "Eliminar";
+            // 
+            // ayudaToolStripMenuItem
+            // 
+            this.ayudaToolStripMenuItem.DropDownItems.AddRange(new System.Windows.Forms.ToolStripItem[] {
+            this.acercaDeToolStripMenuItem});
+            this.ayudaToolStripMenuItem.Name = "ayudaToolStripMenuItem";
+            this.ayudaToolStripMenuItem.Size = new System.Drawing.Size(65, 24);
+            this.ayudaToolStripMenuItem.Text = "Ayuda";
+            // 
+            // acercaDeToolStripMenuItem
+            // 
+            this.acercaDeToolStripMenuItem.Name = "acercaDeToolStripMenuItem";
+            this.acercaDeToolStripMenuItem.Size = new System.Drawing.Size(167, 26);
+            this.acercaDeToolStripMenuItem.Text = "Acerca de...";
+            // 
+            // Form1
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(8F, 16F);
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.ClientSize = new System.Drawing.Size(800, 450);
+            this.Controls.Add(this.BTNactualizar);
+            this.Controls.Add(this.BTNeliminar);
+            this.Controls.Add(this.BTNguardar);
+            this.Controls.Add(this.dataGridView1);
+            this.Controls.Add(this.textBox5);
+            this.Controls.Add(this.textBox4);
+            this.Controls.Add(this.textBox3);
+            this.Controls.Add(this.comboBox1);
+            this.Controls.Add(this.textBox2);
+            this.Controls.Add(this.textBox1);
+            this.Controls.Add(this.label6);
+            this.Controls.Add(this.label5);
+            this.Controls.Add(this.label4);
+            this.Controls.Add(this.label3);
+            this.Controls.Add(this.label2);
+            this.Controls.Add(this.label1);
+            this.Controls.Add(this.menuStrip1);
+            this.MainMenuStrip = this.menuStrip1;
+            this.Name = "Form1";
+            this.Text = "Form1";
+            this.Load += new System.EventHandler(this.Form1_Load);
+            ((System.ComponentModel.ISupportInitialize)(this.dataGridView1)).EndInit();
+            this.menuStrip1.ResumeLayout(false);
+            this.menuStrip1.PerformLayout();
+            this.ResumeLayout(false);
+            this.PerformLayout();
+
+        }
+
+        #endregion
+
+        private System.Windows.Forms.Label label1;
+        private System.Windows.Forms.Label label2;
+        private System.Windows.Forms.Label label3;
+        private System.Windows.Forms.Label label4;
+        private System.Windows.Forms.Label label5;
+        private System.Windows.Forms.Label label6;
+        private System.Windows.Forms.TextBox textBox1;
+        private System.Windows.Forms.TextBox textBox2;
+        private System.Windows.Forms.ComboBox comboBox1;
+        private System.Windows.Forms.TextBox textBox3;
+        private System.Windows.Forms.TextBox textBox4;
+        private System.Windows.Forms.TextBox textBox5;
+        private System.Windows.Forms.DataGridView dataGridView1;
+        private System.Windows.Forms.Button BTNguardar;
+        private System.Windows.Forms.Button BTNeliminar;
+        private System.Windows.Forms.Button BTNactualizar;
+        private System.Windows.Forms.DataGridViewTextBoxColumn Nombre;
+        private System.Windows.Forms.DataGridViewTextBoxColumn Apellido;
+        private System.Windows.Forms.DataGridViewTextBoxColumn Carrera;
+        private System.Windows.Forms.DataGridViewTextBoxColumn CIF;
+        private System.Windows.Forms.DataGridViewTextBoxColumn PromedioGeneral;
+        private System.Windows.Forms.DataGridViewTextBoxColumn MateriasAprobadas;
+        private System.Windows.Forms.MenuStrip menuStrip1;
+        private System.Windows.Forms.ToolStripMenuItem archivoToolStripMenuItem;
+        private System.Windows.Forms.ToolStripMenuItem estudainteToolStripMenuItem;
+        private System.Windows.Forms.ToolStripMenuItem nuevoToolStripMenuItem;
+        private System.Windows.Forms.ToolStripMenuItem salirToolStripMenuItem;
+        private System.Windows.Forms.ToolStripMenuItem guardarToolStripMenuItem;
+        private System.Windows.Forms.ToolStripMenuItem eliminarToolStripMenuItem;
+        private System.Windows.Forms.ToolStripMenuItem ayudaToolStripMenuItem;
+        private System.Windows.Forms.ToolStripMenuItem acercaDeToolStripMenuItem;
+    }
+}
+
---
+++ Registros estudiantes2/Registros estudiantes2/Form1.cs
@@ -0,0 +1,213 @@
+﻿using Microsoft.Win32;
+using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.IO;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+
+namespace Registros_estudiantes2
+{
+    public partial class Form1 : Form
+    {
+        public Form1()
+        {
+            InitializeComponent();
+        }
+
+        private void button1_Click(object sender, EventArgs e)
+        {
+            GrabarDatos();
+            dataGridView1.Rows.Add(textBox1.Text, textBox2.Text, comboBox1.Text, textBox3.Text, textBox4.Text, textBox5.Text);
+            textBox1.Text = "";
+            textBox2.Text = "";
+            comboBox1.Text = "";
+            textBox3.Text = "";
+            textBox4.Text = "";
+            textBox5.Text = "";
+
+        }
+
+        private void GrabarDatos()
+        {
+            try
+            {
+                using (StreamWriter archivo = new StreamWriter("C:\\Users\\johal\\OneDrive\\Documents\\Nueva carpeta\\agenda.txt", true))
+                {
+                    archivo.WriteLine(textBox1.Text);
+                    archivo.WriteLine(textBox2.Text);
+                    archivo.WriteLine(textBox3.Text);
+                    archivo.WriteLine(textBox4.Text); 
+                    archivo.WriteLine(textBox5.Text);
+                    archivo.WriteLine(comboBox1.Text);
+                }
+            }
+            catch (Exception ex)
+            {
+                MessageBox.Show("Error al grabar datos: " + ex.Message);
+            }
+        }
+        private void textBox1_TextChanged(object sender, EventArgs e)
+        {
+
+        }
+
+        private void textBox2_TextChanged(object sender, EventArgs e)
+        {
+
+        }
+
+        private void comboBox1_SelectedIndexChanged(object sender, EventArgs e)
+        {
+
+        }
+
+        private void textBox3_TextChanged(object sender, EventArgs e)
+        {
+
+        }
+
+        private void textBox4_TextChanged(object sender, EventArgs e)
+        {
+
+        }
+
+        private void textBox5_TextChanged(object sender, EventArgs e)
+        {
+
+        }
+
+        private void label3_Click(object sender, EventArgs e)
+        {
+
+        }
+
+        private void salirToolStripMenuItem_Click(object sender, EventArgs e)
+        {
+            var result = MessageBox.Show("¿Estas segura de querer salir?", "Confirmación", MessageBoxButtons.YesNo, MessageBoxIcon.Information);
+
+            if (result == DialogResult.Yes)
+            {
+                Close();
+            }
+            else
+            {
+                return;
+            }
+        }
+
+        private void Form1_Load(object sender, EventArgs e)
+        {
+            if (!File.Exists("C:\\Users\\johal\\OneDrive\\Documents\\Nueva carpeta\\agenda.txt"))
+            {
+                using (TextWriter archivo = new StreamWriter("C:\\Users\\johal\\OneDrive\\Documents\\Nueva carpeta\\agenda.txt"))
+                {
+                    // Se crea el archivo vacio
+                }
+            }
+            else
+            {
+                try
+                {
+                    using (StreamReader archivo = new StreamReader("C:\\Users\\johal\\OneDrive\\Documents\\Nueva carpeta\\agenda.txt"))
+                    {
+                        while (!archivo.EndOfStream)
+                        {
+                            string Nombres = archivo.ReadLine();
+                            string Apellidos = archivo.ReadLine();
+                            string Carrera = archivo.ReadLine();
+                            string CIF = archivo.ReadLine();
+                            string PromedioGeneral = archivo.ReadLine();
+                            string ClasesAprobadas = archivo.ReadLine();
+                            dataGridView1.Rows.Add(Nombres, Apellidos, Carrera, CIF, PromedioGeneral, ClasesAprobadas);
+                        }
+                    }
+                }
+                catch (Exception ex)
+                {
+                    MessageBox.Show("Error al leer datos: " + ex.Message);
+                }
+            }
+        }
+
+        private void BTNeliminar_Click(object sender, EventArgs e)
+        {
+            // Obtener el nombre del estudiante a eliminar
+            string nombreAEliminar = textBox1.Text;
+
+            // Buscar la fila con ese nombre
+            int filaAEliminar = -1;
+            for (int f = 0; f < dataGridView1.Rows.Count; f++)
+            {
+                if (dataGridView1.Rows[f].Cells[0].Value.ToString() == nombreAEliminar)
+                {
+                    filaAEliminar = f;
+                    break;
+                }
+            }
+
+            // Eliminar la fila de la tabla
+            if (filaAEliminar != -1)
+            {
+                dataGridView1.Rows.RemoveAt(filaAEliminar);
+                MessageBox.Show("Se eliminó el estudiante");
+            }
+            else
+            {
+                MessageBox.Show("No se encontró el estudiante");
+            }
+
+            // Eliminar la información del estudiante del archivo de texto
+            try
+            {
+                List<string> lineas = new List<string>();
+                using (StreamReader archivo = new StreamReader("C:\\Users\\johal\\OneDrive\\Documents\\Nueva carpeta\\agenda.txt"))
+                {
+                    while (!archivo.EndOfStream)
+                    {
+                        string nombre = archivo.ReadLine();
+                        if (nombre != nombreAEliminar)
+                        {
+                            lineas.Add(nombre);
+                            lineas.Add(archivo.ReadLine());
+                            lineas.Add(archivo.ReadLine());
+                            lineas.Add(archivo.ReadLine());
+                            lineas.Add(archivo.ReadLine());
+                            lineas.Add(archivo.ReadLine());
+                        }
+                        else
+                        {
+                            // Saltar las 5 líneas correspondientes al estudiante a eliminar
+                            for (int i = 0; i < 5; i++)
+                            {
+                                archivo.ReadLine();
+                            }
+                        }
+                    }
+                }
+
+                // Reescribir el archivo sin la información del estudiante eliminado
+                using (StreamWriter archivo = new StreamWriter("C:\\Users\\johal\\OneDrive\\Documents\\Nueva carpeta\\agenda.txt"))
+                {
+                    foreach (string linea in lineas)
+                    {
+                        archivo.WriteLine(linea);
+                    }
+                }
+            }
+            catch (Exception ex)
+            {
+                MessageBox.Show("Error al eliminar el estudiante del archivo: " + ex.Message);
+            }
+        }
+
+        private void BTNactualizar_Click(object sender, EventArgs e)
+        {
+
+        }
+    }
+}
